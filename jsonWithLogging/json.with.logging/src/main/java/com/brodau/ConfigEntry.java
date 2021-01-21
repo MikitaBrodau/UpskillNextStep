073231f79baa61a6167c8ddcbb06556999bdb1e5
Mikita Brodau <Mikita_Brodau@epam.com>
2021-01-21 14:20:27 +0300

    Done XML config and json output fixed. Decomposition using optimization

--- jsonWithLogging/json.with.logging/src/main/java/com/brodau/ConfigEntry.java
+++ jsonWithLogging/json.with.logging/src/main/java/com/brodau/ConfigEntry.java
@@ -1,12 +1,26 @@
 package com.brodau;
 
+import javax.xml.bind.annotation.*;
+import java.util.Arrays;
 
+@XmlRootElement(name = "configEntry")
+@XmlAccessorType(XmlAccessType.PROPERTY)
 public class ConfigEntry {
-    private final String suffix;
-    private final String[] files;
+    private  String suffix;
+    private  String[] files;
 
     public ConfigEntry(String suffix, String[] files) {
         this.suffix = suffix;
+        this.files = files;
+    }
+    public ConfigEntry(){
+    }
+
+    public void setSuffix(String suffix) {
+        this.suffix = suffix;
+    }
+
+    public void setFiles(String[] files) {
         this.files = files;
     }
 
@@ -16,5 +30,13 @@ public class ConfigEntry {
 
     public String[] getFiles() {
         return files;
+    }
+
+    @Override
+    public String toString() {
+        return "ConfigEntry{" +
+                "suffix='" + suffix + '\'' +
+                ", files=" + Arrays.toString(files) +
+                '}';
     }
 }
